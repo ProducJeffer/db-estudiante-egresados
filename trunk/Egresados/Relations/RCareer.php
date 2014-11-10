cb2a65e90ea1c28805f8f5c7d844ebd6caa72e7e
producjeffer@gmail.com <producjeffer@gmail.com@2924f73a-8c99-4f2c-539e-7c29de00c725>
2014-11-10 16:56:38 +0000

    added the folder to the relationship but the code is not the required

---
+++ trunk/Egresados/Relations/RCareer.php
@@ -0,0 +1,51 @@
+<?php 
+include  "./../protected.php"; /*Valida el inico de sesion*/
+?>
+<!DOCTYPE html>
+<head>
+    <link href="../css/ceGrid.css" rel="stylesheet" type="text/css">
+<link href="../css/main.css" rel="stylesheet" type="text/css">
+<meta charset="UTF-8">
+<title>Lista de Carreras de los estudiantes</title>
+</head>
+
+
+<button class ="btn" id= "atras" type="button" onclick="javascript: location.href='../tool/Menu.php'">Regresar</button>
+
+
+<body>
+<div id="content">
+
+<?php
+
+require '../Conexion/cred.php';
+
+require '../tool/ceSQL.php';
+require '../tool/ceGrid.php';
+
+$dbase = new ceMySQLAdap($server, $usuario, $password,$database);
+
+$dg = new ceDataGrid($dbase); 
+
+$dg->setQuery("EE24COCR,EE25NOCR","ee03crrera");
+
+$dg->allowFilters();
+
+$dg->showCreateButton("href='../formularios/addCareer.php'", ceDataGrid::TYPE_ONCLICK, 'Agregar');
+$dg->setResultsPerPage(10);
+
+$dg->setColumnHeader('EE24COCR', 'Código carrera');
+$dg->setColumnHeader('EE25NOCR', 'Nombre');
+
+
+
+$dg->addStandardControl(ceDataGrid::STDCTRL_EDIT, "href='mt02.php?id=%EE01COCN%'");
+$dg->addStandardControl(ceDataGrid::STDCTRL_DELETE, "href='../acciones/Deletes/deleteCarrera.php?id=%EE24COCR%'");
+$dg->addItem(ceDataGrid::Go_Job, "href='../mod/Estudio.php'");
+
+$dg->showReset("Refrescar");
+$dg->printTable();
+?>
+</div>
+</body>
+</html>
